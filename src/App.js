a9dcdd9dfb31f4f5efd42340c47359ceab76c2a1
Oscar <omherreral81@gmail.com>
2021-12-02 01:12:07 -0500

    resuelto

--- src/App.js
+++ src/App.js
@@ -2,13 +2,28 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.Incrementa = this.Incrementa.bind(this);
+
+    this.state ={
+      counter: 0
+    }
+
+  }
   render() {
     return (
       <div>
-        <span className="value">0</span>
-        <button id="inc">Incrementa</button>
+        <span className="value">{this.state.counter}</span>
+        <button id="inc" onClick={this.Incrementa}>Incrementa</button>
       </div>
     );
+  }
+  Incrementa(){
+    this.setState({
+      counter: this.state.counter +1
+    })
+
   }
 }
 
